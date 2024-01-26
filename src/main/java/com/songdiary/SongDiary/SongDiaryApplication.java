78634c331ab0ffc08ad18638782b47436cd36142
chulsu0012 <81545875+chulsu0012@users.noreply.github.com>
2024-01-26 20:46:33 +0900

    Create basic framework

---
+++ src/main/java/com/songdiary/SongDiary/SongDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.songdiary.SongDiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongDiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/songdiary/SongDiary/SongDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.songdiary.SongDiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
